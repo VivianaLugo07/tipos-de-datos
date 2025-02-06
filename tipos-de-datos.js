ff492303c7ec19812f87611f4917179f95517668
Viviana Lugo <vivianacruz291@gmail.com>
2025-02-06 09:54:05 -0600

    ejemplos de console.log

---
+++ tipos-de-datos.js
@@ -0,0 +1,19 @@
+console.log(typeof 42) //number
+console.log(typeof "Veinticinco") //string
+console.log(typeof -666) //number
+console.log(typeof true) //boolean
+console.log(typeof 0) //number
+console.log(typeof "") //con una sola doble comilla es "invalid"
+console.log(typeof null) //objeto
+console.log(typeof undefined) //undefined
+/*en este ejemplo puse los boolenos con mayúsculas 
+pero como estos son reconocidos en inúsculas, 
+los marca como undefined*/
+console.log(typeof FALSE) //undefined
+console.log(typeof TRUE) //undefined 
+
+console.log(typeof '') //string
+
+/*en este ejemplo puse una comilla doble y una simple 
+pero no se puede, así que lo marca como "Ivalid"*/
+console.log(typeof '"') 
